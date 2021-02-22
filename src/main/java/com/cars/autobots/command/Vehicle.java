54f685339b1ebad506787c27f479e2ec76abb695
aswin1758 <aswinprabhakar1758@gmail.com>
2021-02-22 20:24:18 +0530

    Changes directory structure.Adds h2 as database

---
+++ src/main/java/com/cars/autobots/command/Vehicle.java
@@ -0,0 +1,54 @@
+package com.cars.autobots.vehicle.command;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Vehicle {
+
+    private int numOfWheels;
+    private int engineSize;
+    private String color;
+    private String modelName;
+    private String vehicleType;
+
+    public int getNumOfWheels() {
+        return numOfWheels;
+    }
+
+    public void setNumOfWheels(int numOfWheels) {
+        this.numOfWheels = numOfWheels;
+    }
+
+    public int getEngineSize() {
+        return engineSize;
+    }
+
+    public void setEngineSize(int engineSize) {
+        this.engineSize = engineSize;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+}
